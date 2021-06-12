5e44d545177780fdb71e0eecfb1ac10d0a4b9aa4
petrozavodsky <petrozavodsky@gmail.com>
2021-06-12 20:44:54 +0300

    Add comments

--- src/helpers.go
+++ src/helpers.go
@@ -5,6 +5,7 @@ import (
 	"time"
 )
 
+// Хелперы пакета
 type Helpers struct {
 	min int
 	max int
--- src/show.go
+++ src/show.go
@@ -2,6 +2,7 @@ package main
 
 import "fmt"
 
+// Функция вывода результата
 func View(array [rows][cols]int)  {
 
 	fmt.Println(array)
